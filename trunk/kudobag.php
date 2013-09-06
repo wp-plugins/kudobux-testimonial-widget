18f395bfd5f68850c18f01893ec8eab5914d9850
kudobux <kudobux@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-06 06:08:19 +0000

    adding kudbag
    git-svn-id: https://plugins.svn.wordpress.org/kudobux-testimonial-widget/tags/1.0.1@767705 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/kudobag.php
@@ -0,0 +1,135 @@
+<?php
+
+/*
+Plugin Name: Kudobag
+Plugin URI: http://kudobag.com/
+Description: A simple Social Testimonial Widget
+Version: 1.0
+Author: Kudobag
+Author URI: https://kudobag.com
+License: GPL
+*/
+
+//Adding the Menu Block
+add_action( 'admin_menu', 'register_my_kudobag_menu_page' );
+add_action('admin_menu', 'register_my_kudobag_submenu_page');
+add_action('wp_footer', 'add_widget');
+add_action('admin_head', 'get_id');
+add_action('admin_footer', 'signup_listener');
+$script = "<script src=\"http://kudobag.com/assets/javascripts/kudos/widget.js\"></script><script> Kudos.Widget({uid: '".get_option( 'kudobag_uid' )."'});</script>"; 
+
+function register_my_kudobag_menu_page(){
+    
+    add_menu_page(__('Kudobag_menu','Kudobag'), __('  Kudobag','kudos-menu'), 'manage_options', 'Kudobag', 'kudobag_plugin_default',plugins_url( 'kudobux-testimonial-widget/img/icon.png' ) );
+}
+
+function add_widget(){
+  try{add_option('kudobag_uid', '');}catch(Exception $e){}
+  echo  $GLOBALS['script'];
+}
+
+function register_my_kudobag_submenu_page() {
+add_submenu_page('Kudobag', __('Kudobag-Customize','Sign in'), __('Sign in','kudos-signin'), 'manage_options', 'Sign_in', 'kudobag_sign_in');
+add_submenu_page('Kudobag', __('Kudobag-Customize','Customize Widget'), __('Customize Widget','kudos-widget'), 'manage_options', 'customize-widget', 'kudobag_customize_widget');
+add_submenu_page('Kudobag', __('Kudobag-Customize','Help'), __('Help','kudos-help'), 'manage_options', 'help', 'kudobag_help');
+}
+
+
+//Loading a page in Ifram to help customize the widget
+function kudobag_plugin_default() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+	
+	$url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+	echo '<div class="wrap">';
+	echo '<iframe src="http://kudobag.com/signup/add_channel?platform=1&url='.urlencode($url).'" width="1100" height="900"></iframe>';
+	echo '</div>';
+}
+
+function kudobag_customize_widget() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+	
+	echo '<div class="wrap">';
+	echo '<iframe src="http://kudobag.com/dashboard#customize" width="1020" height="800"></iframe>';
+	echo '</div>';
+}
+
+function kudobag_sign_in() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+	
+	$url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+	echo '<div class="wrap">';
+	echo '<input hidden="input"  id="kudos_id" >';
+echo '<iframe src="http://kudobag.com/login?platform=1&url='.urlencode($url).'" width="1020" height="800"></iframe>';
+	echo '</div>';
+}
+
+function get_id(){
+	echo   '<script type="text/javascript">function insert_id(string){
+		var name=string;
+		document.getElementById("kudos_id").value=name;
+		}
+ </script>';
+}
+
+
+function update_kudos_key( ) {
+  // Error checking
+  if( empty( $_POST ) or $_POST['uid'] == '' or ! $_POST['uid'] ) { print_r(0); exit(); }   
+
+  $new_value = $_POST['uid'];
+
+  // If we successfully update the uid ...
+  if( update_option( "kudobag_uid", $new_value ) ) {
+ $GLOBALS['script'] = "<script src=\"http://kudobag.com/assets/javascripts/kudos/widget.js\"></script><script> Kudos.Widget({uid: '".$new_value ."'});</script>"; 
+  	add_action('wp_footer', 'add_widget');
+    //print_r(1);
+    exit();
+  } 
+  print_r(0);
+  exit();
+}
+
+add_action( "wp_ajax_nopriv_update_kudos_key", "update_kudos_key" ); // Works for users not logged in
+add_action( "wp_ajax_update_kudos_key", "update_kudos_key" ); // Works for users who are logged in
+
+function signup_listener(){
+echo '<script>
+        window.addEventListener( "message",
+          function (e) {
+                if(e.origin !== "http://kudobag.com"){ return; } 
+                $ = jQuery.noConflict();
+ $( document ).ready( function() {
+  $.post( ajaxurl, 
+  { 
+    action: \'update_kudos_key\',
+    uid: e.data
+  }, function( response ) {
+   if( response == 1 ) {
+   }
+   else {
+   }
+  });
+ } );            
+                
+          
+          },
+          false);
+    </script>';
+
+    
+    
+    
+    
+}
+
+
+
+
+
+?>
---
+++ trunk/readme.txt
@@ -0,0 +1,55 @@
+=== Kudobag ===
+Contributors: Kudobag
+Donate link: http://Kudobag.com
+Tags: Testimonials,Kudos,Social Testimonials,Testimonial Widget,Testimonial,Wp Testimonial,Kudobag
+Requires at least: 3.0.1
+Tested up to: 3.6
+Stable tag: 4.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Start amazing your web visitors with social proof of your product or service. Let Kudobag do the job for you.
+== Description ==
+										<p> <b> --Beta-- </b></p>
+<p>Kudobag Testimonial Widget plugin allows you to display social testimonials  or selected quotes on your WordPress blog.</p> 
+<p>Kudobag grabs all the positive and great things people are saying about you on social media(Facebook,Twitter,Blogs,G+,Emails...),
+filters them and display it on your website with an easy to customize clean widget.</p>
+<p>With Kudobag you always get upto date testimonials on your website, this will wow your web visitors and improve your SEO drastically. 
+It is easy to setup and 100% free.</p>
+
+== Installation ==
+
+1. Upload `Kudobag-testimonial-widget` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Visit the Kudobag Menu and start to customize your Kudos
+
+== Frequently Asked Questions ==
+<p>1. How do I customize my widget </p>
+**You can start customization right from wordpress admin, under the Kudobag Menu
+	
+<p>2. When I need help who should I contact </p>
+**We are always more than happy to help. Reach us at support@Kudobag.com
+
+<p>3.How many social accounts can I add </p>
+**You can add as many social accounts as you want
+
+<p>4.Is the plugin free</p>
+**Yes this plugin is 100% free
+
+== Screenshots ==
+
+<p>1.Signup screen where you start your registration process by signing in with Facebook or Twitter</p>
+<p>2.Dashboard area where you select which social feeds you want to show on your website</p>
+<p>3.Widget customization area where you customize the look and feel of your widget</p>
+<p>4.Sample Demo on your blog</p>
+
+== Upgrade Notice ==
+We will always alert you via email or your dashboard when there are new updates and awesome improvements to the product
+- Just do a normal upgrade.
+
+== Changelog ==
+
+= 1.0.1 =
+* None
+
+
