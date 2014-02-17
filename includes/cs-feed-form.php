2744f724980935b399b52300ca57107d9d290825
kudobux <kudobux@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-17 13:47:10 +0000

    updating methods
    git-svn-id: https://plugins.svn.wordpress.org/kudobux-testimonial-widget/trunk@859419 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/cs-feed-form.php
@@ -0,0 +1,18 @@
+<div style="width: 1000px; background: #fff; overflow: hidden" >
+	<iframe src="https://kudobuzz.com/share/<?php echo $GLOBALS['account_name']?>" style="width: 1000px; height: 700px; margin-top: -105px; margin-left: -130px" scrolling="no"></iframe>
+	<!--<form role="form">
+  		<div class="form-group">
+    		<label for="name">Name</label>
+    		<input type="text" class="form-control" id="name" placeholder="User Name">
+  		</div>
+  		<div class="form-group">
+    		<label for="user-url">URL</label>
+    		<input type="text" class="form-control" id="user-url" placeholder="http://">
+  		</div>
+  		<div class="form-group">
+    		<label for="cs-msg">Message</label>
+    		<textarea class="form-control" id="cs-msg" placeholder="Compose your message here" style="height: 100px; resize: none"></textarea>
+  		</div>
+  		<button type="button" class="btn btn-default">Submit</button>
+	</form>-->
+</div>
