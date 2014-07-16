b544d8e7446a0ad2ada6d8362771291b96063569
kudobux <kudobux@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-16 07:47:00 +0000

    adding installation menu4
    git-svn-id: https://plugins.svn.wordpress.org/kudobux-testimonial-widget/trunk@949439 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/after-user-registration.php
+++ includes/after-user-registration.php
@@ -39,7 +39,7 @@ update_option('kudobuzz_uid', $uid);
             <div style="margin: 0px auto 10px auto; width: 820px; overflow: hidden;">
                 
                 <div  style="padding: 0px 20px; margin: 30px auto">
-
+					<h3>
                     <p class="main-title" style="margin-top: 45px; font-size: 30px; color: #585858">Registration Completed Successfully</p>
                     <p>We have sent you an email. Please follow the simple steps below to get started.</p>
                     <ul class="ul-with-bullet">
@@ -52,7 +52,7 @@ update_option('kudobuzz_uid', $uid);
                     </ul>
                     <p>If you need any help reach us at [ hello@kudobuzz.com ]</p>
                     <a style="margin-top: 30px" href="<?php echo get_admin_url() ?>admin.php?page=ModerateReviews" class="btn btn-info btn-lg">Start Publishing Your Testimonials Now</a>
-
+					</h3>
                 </div>
             </div>
         </div>
