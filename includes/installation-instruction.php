45787c6253675c24e2beda211875a251080e482f
kudobux <kudobux@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-24 16:25:11 +0000

    updating instruction
    git-svn-id: https://plugins.svn.wordpress.org/kudobux-testimonial-widget/trunk@954259 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/installation-instruction.php
+++ includes/installation-instruction.php
@@ -1,20 +1,20 @@
 <p></p>
-<h1> How to setup full page testimonial </h1>
 <div>  
     <!-- Full page widget -->
    <p> Upon activating Kudobuzz Plugin we create a Testimonial Page for you by default. You should see this page
     under your pages in wordpress admin dashboard. If for some reason you can't find it follow the steps below to create
     a testimonial page.</p>
-    
-    <p>To setup a Full Page Testimonial</p>
+    <br />
+    <p><h5>To setup a Full Page Testimonial</h5></p>
     <p><ul>
       <p><li> 1. Create a page or post in your Wordpress admin area </li></p>
       <p><li> 2. Copy and paste this short code on the page <b><h2>[kudobuzz-fullpage]</h2></b></li></p>
       <p><li> 3. Save and visit the page to see your testimonials showing </li></p>
       <p><li> 4. If you don't see any testimonials, don't worry head over to the Moderate Reviews area and publish some reviews.</li></p>
      <p></ul></p>
-     
-     <p>To setup a Testimonial Slider</p>
+     <p></p>
+     <br />
+     <p><h5>To setup a Testimonial Slider</h5></p>
     <p><ul>
       <p><li> 1. Go to the edit view of the page you want to embed the slider </li></p>
       <p><li> 2. Copy and paste this short code on the page <b><h2>[kudobuzz-slider]</h2></b></li></p>
